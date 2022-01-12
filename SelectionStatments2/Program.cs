4ceacedba02644aee166ea47fec86632a37e4605
brittany melton <brittanymelton26@gmail.com>
2022-01-12 16:21:35 -0500

    switch case subjects

---
+++ SelectionStatments2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionStatments2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch case exercise 
+
+            Console.WriteLine("what is your favorite subject in school?");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("math is evil");
+                    break;
+                case "literature":
+                    Console.WriteLine("reading is fun");
+                    break;
+                case "science":
+                    Console.WriteLine("science can change the world");
+                    break;
+                case "social studies":
+                    Console.WriteLine("history is important");
+                    break;
+                case "physical education":
+                    Console.WriteLine("physical education helps you stay active!");
+                    break;
+                default:
+                    Console.WriteLine("knowledge is valuable");
+                    break;
+            }
+        }
+    }
+}
